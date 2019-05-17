4194dba8b6c358985c2a0bd616255fa0f53df028
Alee02 <a-shah@hotmail.co.uk>
2019-05-18 00:11:54 +0100

    Advanced Scala Recap

--- src/main/scala/com/dp/scala_recap/AdvancedScalaRecap.scala
+++ src/main/scala/com/dp/scala_recap/AdvancedScalaRecap.scala
@@ -1,5 +1,7 @@
 package com.dp.scala_recap
 
+import scala.concurrent.Future
+
 object AdvancedScalaRecap extends App {
 
   // partial functions
@@ -55,15 +57,48 @@ object AdvancedScalaRecap extends App {
 
   // implicits
 
-  implicit val timeout = 3000
+  implicit val timeout = 50000
 
   def setTimeout(f: () => Unit)(implicit timeout: Int) = f()
 
-  setTimeout(() => println("timeout at "))
+  setTimeout(() => println("timeout at "))(timeout)
 
+
+  // implicit conversions
+  // 1) implicit defs
   case class Person(name: String) {
     def greet = s"Hi, my name is $name"
   }
 
+  implicit def fromStringToPerson(string: String): Person = Person(string)
+
+  "Peter".greet
+
+  implicit class Dog(name: String) {
+    def bark = println("bark!")
+  }
+
+  "Lassie".bark
+  // new Dog("Lassie").bark - automatically done by the compiler
+
+  // organize
+  implicit val inverseOrdering: Ordering[Int] = Ordering.fromLessThan(_ > _)
+  List(1,2,3).sorted // List(3,2,1)
+
+  //imported scope
+  import scala.concurrent.ExecutionContext.Implicits.global
+  val future = Future {
+    println("hello, future")
+  }
+
+  // companion objects of the types included in the call
+  object Person {
+    implicit val personOrdering: Ordering[Person] = Ordering.fromLessThan((a, b) => {
+      a.name.compareTo(b.name) < 0
+    })
+  }
+
+  List(Person("Bob"), Person("Alice")).sorted
+  // List(Person(Alice), Person(Bob))
 
 }
