15abecdf2523893c8e0c3d5de7cef0b343ab7943
Alee02 <a-shah@hotmail.co.uk>
2019-05-22 22:03:20 +0100

    Dynamic programming example

---
+++ src/main/scala/com/dp/actors/InterviewTest.scala
@@ -0,0 +1,18 @@
+package com.dp.actors
+
+object InterviewTest extends App {
+  def findLongSubSeq(s1: String, s2: String): String = {
+//    println("We in")
+    def findLongestSubSeq(s1: String, s2: String, acc: String): String = {
+//      println("We starting")
+      if (s1.isEmpty && s2.isEmpty) acc
+      else if (s1(0) == s2(0)) acc + s"${s1(0)}"
+      else {
+        findLongestSubSeq(s1.dropRight(1), s2.dropRight(1), acc)
+      }
+    }
+    findLongestSubSeq(s1, s1, "")
+  }
+  println("Start")
+  println(findLongSubSeq("ABAC", "CDAB"))
+}
