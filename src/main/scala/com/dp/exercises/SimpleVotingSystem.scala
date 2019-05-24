fe843d45232e654773f9f29be19493c478c7c192
Alee02 <a-shah@hotmail.co.uk>
2019-05-24 18:46:20 +0100

    Simple Voting System WIP

--- src/main/scala/com/dp/exercises/SimpleVotingSystem.scala
+++ src/main/scala/com/dp/exercises/SimpleVotingSystem.scala
@@ -1,5 +1,21 @@
 package com.dp.exercises
 
+import akka.actor.{Actor, ActorRef}
+
 object SimpleVotingSystem extends App {
+
+  case class Vote(candidate: Candidate)
+
+  trait Person
+
+  class Citizen extends Person with Actor {
+    override def receive: Receive = ???
+  }
+
+  class AggregateVotes(citizen: Set[ActorRef]) extends Actor {
+    override def receive: Receive = ???
+  }
+
+  case class Candidate(name: String) extends Person
 
 }
